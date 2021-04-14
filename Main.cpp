23d3be6b16088e5a25773bb26c7db62edf41798e
Squeeshy123 <62818641+Squeeshy123@users.noreply.github.com>
2021-04-14 13:43:39 -0400

    added necessary imgui files

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,15 @@
 #include <SDL/SDL.h>
+#include <vector>
+
+#include "Libs/imgui.h"
+
 #include "Core/Level.h"
 #include "Test/TestLevel.h"
-#include <vector>
+
 int main(int argc, char* argv[]) {
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Window* window = SDL_CreateWindow("Mogue", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0);
@@ -18,11 +22,12 @@ int main(int argc, char* argv[]) {
 
 	while (true)
 	{
+		currentLevel->tick(0.1);
 		// Get the next event
 		SDL_Event event;
 		if (SDL_PollEvent(&event))
 		{
-			currentLevel->tick(0.1);
+			
 
 			if (event.type == SDL_QUIT)
 			{
