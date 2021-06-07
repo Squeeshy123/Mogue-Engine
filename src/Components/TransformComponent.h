0e06bd5a6b9955f83867b8e38e9162c8be4c8fb7
Squeeshy123 <62818641+Squeeshy123@users.noreply.github.com>
2021-06-07 01:28:41 -0400

    Added h and cpp initializations for the ECS

--- src/Components/TransformComponent.h
+++ src/Components/TransformComponent.h
@@ -1,14 +1,20 @@
 #pragma once
+#include "TransformComponent.h"
 
+#include <string>
+
+#include "ECS.h"
 #include "MogueMath.h"
+
+
 using namespace Mogue;
 
 class TransformComponent3D {
-    init_component(TransformComponent3D, 1)
+    init_component_h(TransformComponent3D, 1)
 
-    Vector3 position{0.0f, 0.0f, 0.0f};
-    Vector3 rotation{0.0f, 0.0f, 0.0f};
-    Vector3 scale   {1.0f, 1.0f, 1.0f};
+    public:
+        Vector3 position{0.0f, 0.0f, 0.0f};
+        Vector3 rotation{0.0f, 0.0f, 0.0f};
+        Vector3 scale{1.0f, 1.0f, 1.0f};
 
-    
 };
